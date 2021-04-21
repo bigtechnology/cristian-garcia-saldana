e15dfe9651e8006bf7c12a9da9d12e59e9ddc16d
B-Griffinn <bigriffin94@outlook.com>
2021-04-21 10:19:00 -0700

    Spaced about p-tags for eye appeal

--- src/components/common/About/About.tsx
+++ src/components/common/About/About.tsx
@@ -7,11 +7,13 @@ const About = (): React.ReactElement => {
       <div className="about-content">
         <h3>I&apos;m Cristian Garcia, and this is my story</h3>
         <p>
-          Hi, I&apos;m Cristian Garcia Though my unique background, specialties,
-          and certifications, my passion for fitness and health soars to the
-          highest level. From losing weight, gaining muscle, powerlifting, to
-          just getting you back into your workout routine I guarante my valuable
-          skills and specialties will help you along your own fitness journey.
+          Through my unique background, specialties, and certifications, my
+          passion for fitness and health soars to the highest level. From losing
+          weight, gaining muscle, powerlifting, to just getting you back into
+          your workout routine I guarantee my valuable skills and specialties
+          will help you along your own fitness journey.
+        </p>
+        <p>
           It is my goal...Cristian Saldana-Garcia has made personal fitness and
           training his life&apos;s work. With a pedigree ranging from football,
           basketball, baseball, soccer to weight lifting, Cristian stands
@@ -19,10 +21,13 @@ const About = (): React.ReactElement => {
           leading a healthier and happier life. What started as a favor to
           himself to lose weight and begin living a healthier life has now
           turned into a lifestyle that is obvious but not overwhelming when you
-          meet Cristian. Over the past year he has spent nearly 1,000 hours
-          helping clients reach their individual goals all during a pandemic and
-          he is excited to get back to a more personable level with clients.
-          Cristian is here to help you get started today!
+          meet Cristian.
+        </p>
+        <p>
+          Over the past year he has spent nearly 1,000 hours helping clients
+          reach their individual goals all during a pandemic and he is excited
+          to get back to a more personable level with clients. Cristian is here
+          to help you get started today!
         </p>
       </div>
     </div>
