585bc3fd464db8ca6088d9a1cff41acd08c84d12
B-Griffinn <bigriffin94@outlook.com>
2021-04-21 10:19:28 -0700

    Removed missing carousel img

--- src/components/pages/LandingPage/LandingPageCarousel.tsx
+++ src/components/pages/LandingPage/LandingPageCarousel.tsx
@@ -4,11 +4,10 @@ import React from 'react';
 const LandingPageCarousel = (): React.ReactElement => {
   return (
     <Carousel secondsToChange={5} hideCircles hideArrows>
-      <div title="Irina Turkova Photography" className="item-1"></div>
-      <div title="Kaitlynn Tucker Photography" className="item-2"></div>
-      <div title="Weddings by Scott and Dana" className="item-3"></div>
-      <div className="item-4"></div>
-      <div className="item-5"></div>
+      <div title="" className="item-1"></div>
+      <div title="" className="item-2"></div>
+      <div title="" className="item-3"></div>
+      <div title="" className="item-4"></div>
     </Carousel>
   );
 };
