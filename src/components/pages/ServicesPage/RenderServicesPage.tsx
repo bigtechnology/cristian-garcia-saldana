e192f566d4e17b29befb54cdfa47ea5273e6e09f
B-Griffinn <bigriffin94@outlook.com>
2021-04-21 10:19:45 -0700

    Added & Styled services

--- src/components/pages/ServicesPage/RenderServicesPage.tsx
+++ src/components/pages/ServicesPage/RenderServicesPage.tsx
@@ -3,32 +3,52 @@ import React from 'react';
 const RenderServicesPage = (): React.ReactElement => {
   return (
     <div className="services-wrapper">
+      <h1>Services</h1>
       <div className="services-container">
         <div className="services">
-          <h2>Services</h2>
+          <h2>Training Sessions</h2>
           <div className="service-item">
-            <div>75 min Training Session</div>
+            <div>1 Session</div>
             <div className="space"></div>
-            <div>Contact for pricing</div>
+            <div>$55</div>
           </div>
           <div className="service-item">
-            <div>60 min Training Session</div>
+            <div>4 Sessions</div>
             <div className="space"></div>
-            <div>Contact for pricing</div>
+            <div>$200</div>
           </div>
           <div className="service-item">
-            <div>12 Week Training Program</div>
+            <div>8 Sessions</div>
             <div className="space"></div>
-            <div>Contact for pricing</div>
+            <div>$325</div>
           </div>
           <div className="service-item">
-            <div>8 Week Training Program</div>
+            <div>12 Sessions</div>
             <div className="space"></div>
-            <div>Contact for pricing</div>
+            <div>$450</div>
+          </div>
+
+          <h2>Training Programs</h2>
+          <div className="service-item">
+            <div>8 Week Program</div>
+            <div className="space"></div>
+            <div>$180</div>
+          </div>
+          <div className="service-item">
+            <div>Returning 12 Week Program</div>
+            <div className="space"></div>
+            <div>$200</div>
+          </div>
+          <div className="service-item">
+            <div>12 Week Program</div>
+            <div className="space"></div>
+            <div>$230</div>
           </div>
           <p>
-            For other services and questions please contact us{' '}
-            <a href="mailto:cgsaldanasbd@gmail.com">here</a>
+            For other services and questions please contact me{' '}
+            <a href="mailto:cgsaldanasbd@gmail.com?subject=Contact Cristian Garcia Fitness">
+              here
+            </a>
           </p>
           {/* <p>*Includes </p>
           <p>**Per</p> */}
